b07c057d899d7f785c07dda3bc69f6339ff5d82c
Thorben von Rekowski <rekowski.thorben@th0bse.com>
2024-07-19 12:07:12 +0200

    feat: add copilotchat and basic config for that
    Add the CopilotChat plugin and some really basic config for it.
    More involved config, keybinds, custom prompts etc. may still be needed,
    right now it is not really convenient to use.

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,11 @@
+return {
+    "copilotc-nvim/copilotchat.nvim",
+    branch = "canary",
+    dependencies = {
+        { "zbirenbaum/copilot.lua" },
+        { "nivm-lua/plenary.nvim" },
+    },
+    opts = {
+        debug = true,
+    },
+}
